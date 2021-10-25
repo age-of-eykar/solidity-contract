110ae7143dc4abeed79e294364a0a7442e040bb2
Thomas Marchand <me@thomas-marchand.com>
2021-10-25 20:42:02 +0200

    introduce convert tools

--- contracts/CoordinatesLib.sol
+++ contracts/CoordinatesLib.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.9 <0.9.0;
 
-library CoordinatesLib{
+library CoordinatesLib {
     function split(bytes32 source) public pure returns (bytes16 x, bytes16 y) {
         bytes16[2] memory output = [bytes16(0), 0];
         assembly {
@@ -11,15 +11,35 @@ library CoordinatesLib{
         return (output[0], output[1]);
     }
 
+    function merge(bytes16 sourceA, bytes16 sourceB)
+        public
+        pure
+        returns (bytes32)
+    {
+        return bytes32((uint256(uint128(sourceA)) << 128) | uint128(sourceB));
+    }
+
     function convertToCoordinates(bytes32 input)
         public
         pure
-        returns (int64 x, int64 y)
-    {}
+        returns (int128 x, int128 y)
+    {
+        bytes16 xbytes;
+        bytes16 ybytes;
+        (xbytes, ybytes) = split(input);
 
-    function convertFromCoordinates(int64 x, int64 y)
+        uint128 ux = uint128(xbytes);
+        uint128 uy = uint128(ybytes);
+
+        x = int128(ux);
+        y = int128(uy);
+    }
+
+    function convertFromCoordinates(int128 x, int128 y)
         public
         pure
         returns (bytes32 output)
-    {}
+    {
+        return merge(bytes16(uint128(x)), bytes16(uint128(y)));
+    }
 }
--- test/coordinates_lib.js
+++ test/coordinates_lib.js
@@ -7,4 +7,17 @@ contract('CoordinatesLib', (accounts) => {
         assert.equal(result.x, '0x01234567890123450123456789012345')
         assert.equal(result.y, '0x00000000000000000000000000000000')
     });
+
+    it('should extract bytes32 to two coordinates', async () => {
+        const instance = await CoordinatesLib.deployed();
+        const result = await instance.convertToCoordinates('0x00000000000000000000000000000001ffffffffffffffffffffffffffffffff');
+        assert.equal(result.x.toString(10), "1");
+        assert.equal(result.y.toString(10), "-1");
+    });
+
+    it('should merge two coordinates in one bytes32', async () => {
+        const instance = await CoordinatesLib.deployed();
+        const result = await instance.convertFromCoordinates(1, -1);
+        assert.equal(result, '0x00000000000000000000000000000001ffffffffffffffffffffffffffffffff');
+    });
 })
