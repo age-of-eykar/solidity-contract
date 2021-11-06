b8766896f07643246bea44e2955d2dc12ef4a13c
Thomas Marchand <me@thomas-marchand.com>
2021-11-06 11:37:38 +0100

    improve conquer function

--- contracts/Eykar.sol
+++ contracts/Eykar.sol
@@ -23,10 +23,11 @@ contract Eykar {
         string name;
         address owner; // owner is a player
         bytes32 location; // place of power
+        uint64 plotsAmount;
         uint256 people;
         uint256 food;
         uint256 materials;
-        int256 redirection; // id should be your own if there is no redirection
+        uint256 redirection; // id should be your own if there is no redirection
     }
 
     // all redeemed plots on the map
@@ -37,6 +38,23 @@ contract Eykar {
 
     // colonies id per player address
     mapping(address => uint256[]) public coloniesPerPlayer;
+
+    /**
+     * Returns the colony struct for the given colony id
+     * @param colonyId the colony id
+     * @return colony struct after redirections
+     */
+    function getColony(uint256 colonyId)
+        public
+        view
+        returns (Colony memory colony)
+    {
+        colony = colonies[colonyId - 1];
+        while (colony.redirection != colonyId) {
+            colonyId = colony.redirection;
+            colony = colonies[colonyId - 1];
+        }
+    }
 
     /**
      * Allows a player to conquer a new plot
@@ -50,17 +68,38 @@ contract Eykar {
     {
         Colony memory colony = colonies[colonyId - 1];
         require(msg.sender == colony.owner);
-
-        uint256 distanceLength = CoordinatesLib.distance(
-            colony.location,
-            location
+        (int128 x1, int128 y1) = CoordinatesLib.convertToCoordinates(
+            colony.location
         );
+        (int128 x2, int128 y2) = CoordinatesLib.convertToCoordinates(location);
+        uint256 distanceLength = CoordinatesLib.distance(x1, y1, x2, y2);
         arrivalDate = distanceLength * 8 + block.timestamp;
 
         require(
             map[location].owner == 0 ||
                 map[location].dateOfOwnership > arrivalDate
         );
+
+        // we can have only 4 surrounding different colonies
+        Colony[] memory detectedColonies = new Colony[](4);
+        uint8 size = 0;
+
+        for (int8 i = -1; i <= 1; i++)
+            for (int8 j = -1; j <= 1 && !(i == j && i == 0); j++) {
+                Plot memory plot = map[
+                    CoordinatesLib.convertFromCoordinates(x2 + i, y2 + j)
+                ];
+
+                // Insert colonies to detectedColonies so it doesn't contain duplicate value
+                bool found = false;
+                Colony memory foundColony = getColony(plot.owner);
+                for (uint8 k = 0; found == false && k < size; k++)
+                    if (
+                        detectedColonies[k].redirection ==
+                        foundColony.redirection
+                    ) found = true;
+                if (!found) detectedColonies[size++] = foundColony;
+            }
 
         // todo: find real colonyId (create it if needed?)
         map[location] = Plot(colonyId, arrivalDate, StructureType.SettlerCamp);
